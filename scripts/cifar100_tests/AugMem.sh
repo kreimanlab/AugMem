2771082bb920605ad8f7ec1986c9d0db2f6b7f27
MorganBDT <morgan_talbot@alumni.brown.edu>
2021-11-08 12:17:59 -0500

    Augmem script prep for cifar100

--- scripts/cifar100_tests/AugMem.sh
+++ scripts/cifar100_tests/AugMem.sh
@@ -12,7 +12,7 @@ elif [ "$DATASET" = "ilab2mlight" ]; then
     DATAROOT="/media/data/Datasets/ilab2M/iLab-2M-Light"
     #DATAROOT="/media/mengmi/KLAB15/Mengmi/proj_CL_NTM/data/ilab/iLab-2M-Light/"
 elif [ "$DATASET" = "cifar100" ]; then
-    DATAROOT="/media/data/morgan_data/cifar100"
+    DATAROOT="./data/cifar100"
 else
     echo "Invalid dataset name!"
     exit
@@ -21,6 +21,6 @@ fi
 mkdir -p ${OUTDIR}/class_iid/AugMem_ResNet18/
 mkdir -p plots
 
-python -u experiment_aug.py --scenario class_iid --dataset $DATASET --dataroot $DATAROOT --lr 0.001 --replay_coef 5 --first_times 10 --replay_times 1 --reg_coef 1000 --n_epoch 1  --memory_size 200 --freeze_feature_extract --n_runs 1 --model_type resnet --model_name ResNet18 --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 --output_dir $OUTDIR | tee ${OUTDIR}/class_iid/AugMem_ResNet18/log.log
+python -u experiment_aug.py --scenario class_iid --dataset $DATASET --dataroot $DATAROOT --lr 0.0001 --replay_coef 5 --first_times 10 --replay_times 1 --reg_coef 1000 --n_epoch 1  --memory_size 200 --freeze_feature_extract --n_runs 2 --model_type resnet --model_name ResNet18 --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 --output_dir $OUTDIR | tee ${OUTDIR}/class_iid/AugMem_ResNet18/log.log
 python -u plot.py --n_class_per_task 5 --scenario class_iid --output_dir $OUTDIR --result_dir AugMem_ResNet18
 mv plots/AugMem_class_iid.png ${OUTDIR}/class_iid/AugMem_ResNet18/AugMem_class_iid.png
