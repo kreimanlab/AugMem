80c6a08a68d7c97fecf87d763741e6a9bb0c5d7c
MorganBDT <morgan_talbot@alumni.brown.edu>
2021-11-09 10:16:22 -0500

    setup script for hyperparam tuning

--- scripts/cifar100_tests/AugMem.sh
+++ scripts/cifar100_tests/AugMem.sh
@@ -22,11 +22,11 @@ lr=0.0001
 mem_sparse=1
 memory_Nslots=100
 
-OUTDIR="${OUTDIR}_lr_${lr}_memsparse_${mem_sparse}_memNslots_${memory_Nslots}"
-
-mkdir -p ${OUTDIR}/class_iid/AugMem_ResNet18/
+OUTDIR=augmem_gridsearch_cifar100
+custom_folder="AugMem_lr_${lr}_memsparse_${mem_sparse}_memNslots_${memory_Nslots}"
+mkdir -p ${OUTDIR}/class_iid/${custom_folder}
 mkdir -p plots
 
-python -u experiment_aug.py --scenario class_iid --dataset $DATASET --dataroot $DATAROOT --n_runs 2 --n_epoch 1 --n_epoch_first_task 10 --lr ${lr} --mem_sparse ${mem_sparse} --memory_Nslots ${memory_Nslots} --replay_times 1 --replay_coef 5 --reg_coef 1000 --memory_size 200 --freeze_feature_extract --model_type resnet --model_name ResNet18 --pretrained  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 --output_dir $OUTDIR | tee ${OUTDIR}/class_iid/AugMem_ResNet18/log.log
-python -u plot.py --n_class_per_task 5 --scenario class_iid --output_dir $OUTDIR --result_dir AugMem_ResNet18
-mv plots/AugMem_class_iid.png ${OUTDIR}/class_iid/AugMem_ResNet18/AugMem_class_iid.png
+python -u experiment_aug.py --scenario class_iid --dataset $DATASET --dataroot $DATAROOT --n_runs 2 --n_epoch 1 --n_epoch_first_task 10 --lr ${lr} --mem_sparse ${mem_sparse} --memory_Nslots ${memory_Nslots} --replay_times 1 --replay_coef 5 --reg_coef 1000 --memory_size 200 --freeze_feature_extract --model_type resnet --model_name ResNet18 --pretrained  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 --output_dir ${OUTDIR} --custom_folder ${custom_folder} | tee ${OUTDIR}/class_iid/${custom_folder}/log.log
+python -u plot.py --n_class_per_task 5 --scenario class_iid --output_dir $OUTDIR --result_dir ${custom_folder}
+mv plots/AugMem_class_iid.png ${OUTDIR}/class_iid/${custom_folder}/AugMem_class_iid.png
