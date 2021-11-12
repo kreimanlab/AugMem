11ece2c99ac864a3accb5dab875189c429686a77
MorganBDT <morgan_talbot@alumni.brown.edu>
2021-11-11 19:48:54 -0500

    10 runs of AGEM

--- scripts/cifar100_tests/AGEM.sh
+++ scripts/cifar100_tests/AGEM.sh
@@ -23,6 +23,6 @@ DATAROOT=${3:-${DDATAROOT}}
 mkdir -p ${OUTDIR}/class_iid/AGEM_ResNet18/
 mkdir -p plots
 
-python -u experiment.py --scenario class_iid --dataset $DATASET --dataroot $DATAROOT  --lr 0.0001 --n_epoch_first_task 10 --n_runs 2 --model_type resnet --model_name ResNet18 --pretrained --agent_type exp_replay --agent_name AGEM  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 --output_dir $OUTDIR --keep_best_task1_net | tee ${OUTDIR}/class_iid/AGEM_ResNet18/log.log
+python -u experiment.py --scenario class_iid --dataset $DATASET --dataroot $DATAROOT  --lr 0.0001 --n_epoch_first_task 10 --n_runs 10 --model_type resnet --model_name ResNet18 --pretrained --agent_type exp_replay --agent_name AGEM  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 --output_dir $OUTDIR --keep_best_task1_net | tee ${OUTDIR}/class_iid/AGEM_ResNet18/log.log
 python -u plot.py --n_class_per_task 5 --scenario class_iid --output_dir $OUTDIR --result_dir AGEM_ResNet18
 mv plots/AGEM_class_iid.png ${OUTDIR}/class_iid/AGEM_ResNet18/AGEM_class_iid.png
