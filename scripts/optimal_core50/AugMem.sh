2d8582b424d0669298bd950dbcacb909eb9932c3
MorganBDT <morgan_talbot@alumni.brown.edu>
2021-11-23 19:06:50 -0500

    update script

--- scripts/optimal_core50/AugMem.sh
+++ scripts/optimal_core50/AugMem.sh
@@ -7,7 +7,7 @@ OUTDIR="${DATASET}_outputs"
 GPU="${2:-0}"
 
 if [ "$DATASET" = "core50" ]; then
-    DATAROOT="/media/mengmi/KLAB15/Mengmi/proj_CL_NTM/data/core50"
+    DATAROOT="/media/data/Datasets/Core50"
 elif [ "$DATASET" = "toybox" ]; then
     DATAROOT="/media/data/morgan_data/toybox/images"
 elif [ "$DATASET" = "ilab2mlight" ]; then
@@ -18,30 +18,16 @@ else
     exit
 fi
 
-#mkdir -p $OUTDIR/iid/AugMem_ResNet18/
-mkdir -p $OUTDIR/class_iid/AugMem_ResNet18/
-#mkdir -p $OUTDIR/instance/AugMem_ResNet18/
-mkdir -p $OUTDIR/class_instance/AugMem_ResNet18/
+#mkdir -p $OUTDIR/iid/AugMem_SqueezeNet/
+mkdir -p $OUTDIR/class_iid/AugMem_SqueezeNet/
+#mkdir -p $OUTDIR/instance/AugMem_SqueezeNet/
+mkdir -p $OUTDIR/class_instance/AugMem_SqueezeNet/
 
-#(224*224*3+10)*1200/[ ((64*13*13)+10)*400+1*250*8 ] = 83
-#[ ((64*13*13)+10)*400+1*250*8 ]/(224*224*3+10) = 28
+#python -u experiment.py --scenario iid --reg_coef 1000 --output_dir outputs_rohil --n_epoch 1 --memory_size 200 --freeze_feature_extract --n_runs 10 --model_type resnet --memory_size 200 --model_name SqueezeNet --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid 0 --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 20 | tee ${OUTDIR}/iid/AugMem_SqueezeNet/log.log	#&
 
-#--freeze_feature_extract
-#--freeze_batchnorm
-#--freeze_memory
-#--model_weights ./pretrain/cifar
-#--freeze_memory
-#--visualize
-#python -u experiment.py --scenario iid --reg_coef 1000 --output_dir outputs_rohil --n_epoch 1 --memory_size 200 --freeze_feature_extract --n_runs 10 --model_type resnet --memory_size 200 --model_name ResNet18 --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid 0 --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 20 | tee ${OUTDIR}/iid/AugMem_ResNet18/log.log	#&
+#python -u experiment.py --scenario instance --reg_coef 1000 --output_dir outputs_rohil --n_epoch 1 --memory_size 200 --model_weights ./pretrain/cifar --freeze_feature_extract --memory_size 200 --n_runs 10 --model_type resnet --model_name SqueezeNet --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid 0 --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 20 | tee ${OUTDIR}/instance/AugMem_SqueezeNet/log.log    #&
 
-#python -u experiment.py --scenario instance --reg_coef 1000 --output_dir outputs_rohil --n_epoch 1 --memory_size 200 --model_weights ./pretrain/cifar --freeze_feature_extract --memory_size 200 --n_runs 10 --model_type resnet --model_name ResNet18 --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid 0 --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 20 | tee ${OUTDIR}/instance/AugMem_ResNet18/log.log    #&  
-    
-#--visualize
-#enable this flag to visualize hypothesis and tsne plots
-python -u experiment_aug.py --scenario class_iid --replay_coef 5 --dataset $DATASET --dataroot $DATAROOT  --output_dir $OUTDIR  --first_times 1 --replay_times 1 --reg_coef 1000 --n_epoch 1  --memory_size 200 --freeze_feature_extract --n_runs 10 --model_type resnet --model_name ResNet18 --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 | tee ${OUTDIR}/class_iid/AugMem_ResNet18/log.log    #&            
+python -u experiment_aug.py --scenario class_iid --n_runs 10 --n_epoch_first_task 10 --n_epoch 1 --lr 0.001 --memory_Nslots 100  --memory_Nfeat 8 --memory_size 200 --replay_times 1 --replay_coef 5 --reg_coef 1000 --freeze_feature_extract --model_type resnet --model_name SqueezeNet --pretrained --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --dataset $DATASET --dataroot $DATAROOT  --output_dir $OUTDIR --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 | tee ${OUTDIR}/class_iid/AugMem_SqueezeNet/log.log    #&
 
-#--freeze_feature_extract
-#--visualize
-#enable this flag to visualize hypothesis and tsne plots
-python -u experiment_aug.py --freeze_feature_extract --lr 0.00001 --first_times 3 --dataset $DATASET --dataroot $DATAROOT  --output_dir $OUTDIR --replay_coef 5 --replay_times 1 --memory_size 200 --scenario class_instance --reg_coef 1000 --n_epoch 1 --n_runs 10 --model_type resnet --model_name ResNet18 --pretrained --memory_Nslots 100  --memory_Nfeat 8 --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 | tee ${OUTDIR}/class_instance/AugMem_ResNet18/log.log    #&
 
+python -u experiment_aug.py --scenario class_instance --n_runs 10 --n_epoch_first_task 10 --n_epoch 1 --lr 0.00025 --memory_Nslots 100  --memory_Nfeat 8 --memory_size 200 --replay_times 1 --replay_coef 5 --reg_coef 1000 --freeze_feature_extract --model_type resnet --model_name SqueezeNet --pretrained --agent_type aug_mem --agent_name AugMem  --gpuid $GPU --dataset $DATASET --dataroot $DATAROOT  --output_dir $OUTDIR --momentum 0.9 --weight_decay 0.0001 --batch_size 21 --n_workers 8 | tee ${OUTDIR}/class_instance/AugMem_SqueezeNet/log.log    #&
